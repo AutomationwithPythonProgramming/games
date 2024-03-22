9a04b57e6ae20aa6e5d13b17bd8e7da68bbbf355
shanthan-rrx <148105937+shanthan-rrx@users.noreply.github.com>
2024-03-22 10:31:32 -0400

    Update roulette.py - added funation

--- roulette.py
+++ roulette.py
@@ -1,18 +1,25 @@
 import random
 
-number = random.randint(0,36)
-print('Welcome, Please select your number from 0 to 36')
+def Play():
+    print('Welcome, Please select your number from 0 to 36')
 
-selectNum = input()
-# selectNum = int(selectNum)
+    number = random.randint(0,36)
+    selectNum = input()
 
-if int(selectNum) > 36:
-    print('Invalid, re-enter number from 0 to 36')
+    if int(selectNum) > 36:
+        print('Invalid, re-enter number from 0 to 36')
 
-elif int(selectNum) == number:
-    number = str(number)
-    print('You Won! The winning number is ' + number)
+    elif int(selectNum) == number:
+        number = str(number)
+        print('You Won! The winning number is ' + number)
 
-elif int(selectNum) != number:
-    number = str(number)
-    print('You Lost! The winning number is ' + number)
+    elif int(selectNum) != number:
+        number = str(number)
+        print('You Lost! The winning number is ' + number)
+
+Retry = 'Yes'
+while Retry == 'Yes':
+    Play()
+
+    print('Do you want to play again ? (Yes or No)')
+    Retry = input()
