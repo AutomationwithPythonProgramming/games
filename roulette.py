7f6a61372d409a426db40063e62a617d79709b5f
shanthan-rrx <148105937+shanthan-rrx@users.noreply.github.com>
2024-03-22 13:05:08 -0400

    Update roulette.py - colorama

--- roulette.py
+++ roulette.py
@@ -1,25 +1,26 @@
 import random
+from colorama import Fore
 
 def Play():
-    print('Welcome, Please select your number from 0 to 36')
-
+    print(Fore.WHITE + 'Welcome, Please select your number from 0 to 36')
+    
     number = random.randint(0,36)
     selectNum = input()
 
     if int(selectNum) > 36:
-        print('Invalid number entered')
+        print(Fore.BLUE + 'Invalid number entered')
 
     elif int(selectNum) == number:
         number = str(number)
-        print('You Won! The winning number is ' + number)
+        print(Fore.GREEN + 'You Won! The winning number is ' + number)
 
     elif int(selectNum) != number:
         number = str(number)
-        print('You Lost! The winning number is ' + number)
+        print(Fore.RED + 'You Lost! The winning number is ' + number)
 
 Retry = 'Yes'
 while Retry == 'Yes':
     Play()
 
-    print('Do you want to play again ? (Yes or No)')
+    print(Fore.WHITE + 'Do you want to play again ? (Yes or No)')
     Retry = input()
