4f5373c4dc4b1adfae6bbf181e0645544d9f003a
shanthan-rrx <148105937+shanthan-rrx@users.noreply.github.com>
2024-03-22 22:17:36 -0400

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,10 @@
-
+''' 
+Logic
+House plays - card 1
+Player plays - card 1
+House plays - card 2 --> if close to 21 stop and flip card
+Player plays - card 2 --> if close to 21 stop
+If not close to 21
+House plays - card 3 --> if close to 21 stop and flip card or busted
+House plays - card 3 --> if close to 21 stop or busted
+'''
