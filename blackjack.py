21dfad6f5eb7ef1c9d7d3860baa20e65a156a10f
shanthan-rrx <148105937+shanthan-rrx@users.noreply.github.com>
2024-03-27 10:01:22 -0400

    Delete blackjack.py

--- blackjack.py
+++
@@ -1,9 +0,0 @@
-''' 
-House plays - card 1 - close card
-Player plays - card 1
-House plays - card 2 --> if close to 21 stop and flip card
-Player plays - card 2 --> if close to 21 stop
-If not close to 21
-House plays - card 3 --> if close to 21 stop and flip card or busted
-House plays - card 3 --> if close to 21 stop or busted
-'''
